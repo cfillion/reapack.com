49fe0e3b6d449973216d28f1a02d5eb3f47dafa4
cfillion <cfillion@users.noreply.github.com>
2016-06-09 20:35:26 -0400

    count downloads, and debug page

--- app.rb
+++ app.rb
@@ -18,7 +18,9 @@ class ReaPack::WebApp < Sinatra::Base
   URL = 'https://api.github.com/repos/cfillion/reapack/releases'
 
   def initialize
-    @@lastUpdate = Time.new 0
+    @@boot_time = Time.now
+    @@last_update = Time.new 0
+    @@last_response = nil
     @@latest = nil
     super
   end
@@ -31,16 +33,19 @@ class ReaPack::WebApp < Sinatra::Base
 
   def update
     now = Time.now
-    return if @@lastUpdate > now - 3600
+    return if @@last_update > now - 3600
 
-    @@lastUpdate = now
+    @@last_update = now
 
     req = make_request
     req.errback {
+      @@last_response = req.error
     }
 
     req.callback {
       next unless req.response_header.status == 200
+      @@last_response = req.response_header
+
       releases = []
       req.response.each {|json_release|
         release = make_release json_release
@@ -59,17 +64,21 @@ class ReaPack::WebApp < Sinatra::Base
     release.name = json['tag_name'].to_s
     release.author = json['author'].to_h['login'].to_s
     release.time = DateTime.rfc3339 json['published_at']
+    release.downloads = 0
 
-    json['assets'].to_a.each {|asset|
-      case asset['name']
+    json['assets'].to_a.each {|json_asset|
+      asset = make_asset json_asset
+      release.downloads += asset.downloads
+
+      case asset.name
       when 'reaper_reapack32.dylib'
-        release.darwin32 = make_asset asset
+        release.darwin32 = asset
       when 'reaper_reapack64.dylib'
-        release.darwin64 = make_asset asset
+        release.darwin64 = asset
       when 'reaper_reapack32.dll'
-        release.win32 = make_asset asset
+        release.win32 = asset
       when 'reaper_reapack64.dll'
-        release.win64 = make_asset asset
+        release.win64 = asset
       end
     }
 
@@ -78,28 +87,39 @@ class ReaPack::WebApp < Sinatra::Base
 
   def make_asset(json)
     asset = OpenStruct.new
+    asset.name = json['name']
     asset.url = json['browser_download_url']
     asset.size = Filesize.from('%d B' % json['size'].to_i)
+    asset.downloads = json['download_count'].to_i
 
     asset
   end
 
   def harvest_data(releases)
     return if releases.empty?
+
     @@latest = releases.first
+    @@downloads = releases.map {|r| r.downloads }.inject(&:+)
 
     [:darwin32, :darwin64, :win32, :win64].each {|var|
       next if @@latest[var]
       match = releases.find {|r| r[var] }
-      @@latest[var] = match[var]
+      @@latest[var] = match[var] if match
     }
   end
 
   use Sass::Plugin::Rack
 
   get '/' do
-    @latest = @@latest or update
-    last_modified @@lastUpdate if @latest
+    if @@latest
+      last_modified @@last_update
+      @latest = @@latest
+      p @latest
+      @downloads = @@downloads
+    else
+      update
+    end
+
     slim :index
   end
 
@@ -107,6 +127,24 @@ class ReaPack::WebApp < Sinatra::Base
     update
     content_type 'text/plain'
     'Hello GitHub, refreshing the release feed now...'
+  end
+
+  get '/debug' do
+    content_type 'text/plain'
+    headers['X-Last-Update'] = @@last_update.to_s
+    headers['X-Server-Started'] = @@boot_time.to_s
+
+    case @@last_response
+    when NilClass
+      [200, 'No request made yet.']
+    when String
+      [500, @@last_response]
+    else
+      [
+        @@last_response.status,
+        @@last_response.map {|k, v| "%s: %s\n" % [k, v] }
+      ]
+    end
   end
 
   not_found do
