1f24aa079599e53cb134b315030d51a963ac67e9
cfillion <cfillion@users.noreply.github.com>
2017-01-09 06:54:20 -0500

    migrate from sinatra to static website using middleman

---
+++ config.rb
@@ -0,0 +1,9 @@
+require 'slim'
+
+activate :asset_hash
+activate :directory_indexes
+activate :minify_css
+activate :minify_javascript
+
+set :slim, disable_escape: false, use_html_safe: false
+set :trailing_slash, false
