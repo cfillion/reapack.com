f0395af6535a0f74c7e89ea92aa9fbb472198bb3
cfillion <cfillion@users.noreply.github.com>
2017-11-09 23:25:36 -0500

    remove "edit this page" link
    It could cause confusion with the "Edit this list" button in the
    repository list page.

--- config.rb
+++ config.rb
@@ -13,11 +13,3 @@ set :trailing_slash, false
 
 set :software_requirements, 'REAPER v4.7+ (v5.12 or later recommended).'
 set :system_requirements, 'At least macOS 10.7, Windows Vista or Wine 1.8.'
-
-helpers do
-  def current_source_file
-    root = Pathname.new app.root
-    source_file = Pathname.new current_page.source_file
-    source_file.relative_path_from root
-  end
-end
