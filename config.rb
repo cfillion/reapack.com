761636fd0518fa2e22f8470990a76178bc1b72eb
cfillion <cfillion@users.noreply.github.com>
2017-11-11 18:38:20 -0500

    move the domain name to the confguration file

--- config.rb
+++ config.rb
@@ -11,4 +11,6 @@ activate :minify_javascript
 set :slim, disable_escape: false, use_html_safe: false
 set :trailing_slash, false
 
+set :host, 'https://reapack.com'
+
 page "release-notes/*", layout: :release_notes_layout
