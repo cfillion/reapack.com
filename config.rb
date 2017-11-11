bbb3707650ae3fe31376f9aca0fd3d1638a62bac
cfillion <cfillion@users.noreply.github.com>
2017-11-10 21:36:41 -0500

    add v1.1 release notes

--- config.rb
+++ config.rb
@@ -13,3 +13,13 @@ set :trailing_slash, false
 
 set :software_requirements, 'REAPER v4.7+ (v5.12 or later recommended).'
 set :system_requirements, 'At least macOS 10.7, Windows Vista or Wine 1.8.'
+
+page "release-notes/*", layout: :release_notes_layout
+
+helpers {
+  def release_notes
+    sitemap.resources
+      .select {|page| page.path.start_with? 'release-notes/' }
+      .sort_by {|p| Gem::Version.new p.data.version }
+  end
+}
