dffa41f4a71dd86f282e89ac316d9ca95ad31cbf
cfillion <cfillion@users.noreply.github.com>
2017-11-11 13:44:50 -0500

    automatically format release notes forum/issue links

--- config.rb
+++ config.rb
@@ -12,11 +12,3 @@ set :slim, disable_escape: false, use_html_safe: false
 set :trailing_slash, false
 
 page "release-notes/*", layout: :release_notes_layout
-
-helpers {
-  def release_notes
-    sitemap.resources
-      .select {|page| page.path.start_with? 'release-notes/' }
-      .sort_by {|p| Gem::Version.new p.data.version }
-  end
-}
---
+++ helpers/release_notes.rb
@@ -0,0 +1,22 @@
+module ReleaseNotes
+  def all_release_notes
+    sitemap.resources
+      .select {|page| page.path.start_with? 'release-notes/' }
+      .sort_by {|p| Gem::Version.new p.data.version }
+  end
+
+  def format_release_notes(release_notes)
+    release_notes = release_notes.to_str
+
+    release_notes.gsub!(/\[(([tp])=\d+)\]/) {
+      title = $2 == 't' ? 'forum thread' : 'forum post'
+      yield title, "https://forum.cockos.com/showthread.php?#{$1}"
+    }
+
+    release_notes.gsub!(/\[#(\d+)\]/) {
+      yield 'GitHub issue', "https://github.com/cfillion/reapack/issues/#{$1}"
+    }
+
+    release_notes
+  end
+end
