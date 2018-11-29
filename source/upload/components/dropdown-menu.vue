cfillion/reapack.com

commit 243de32889bde1a9baac76969fd20d1771bbf118

close dropdown menus when clicking to the right of the dropdown button

source/upload/components/dropdown-menu.vue
@@ -33,7 +33,7 @@ export default
       f = if add then document.addEventListener else document.removeEventListener
       f 'click', @onDocumentClick, true
     onDocumentClick: (event) ->
-      if @show && (!@button?.contains(event.target))
+      if @show && !@button?.contains(event.target)
         event.preventDefault()
         @$emit 'leave'
   watch:

source/upload/components/input-dropdown.vue
@@ -1,7 +1,7 @@
 <template lang="slim">
 .dropdown
   button (
-    :id="id" type="button" :class="{ active: showMenu }"
+    :id="id" ref="button" type="button" :class="{ active: showMenu }"
     :disabled="disabled" @click="showMenu = !showMenu"
   )
     .placeholder v-if="!displayValue" Select a value…
@@ -10,7 +10,7 @@
       | {{ displayValue }}
     i.caret.fa.fa-caret-down
   dropdown-menu (
-    :items="choices" :show="showMenu" :multiple="multiple" :button="$el"
+    :items="choices" :show="showMenu" :multiple="multiple" :button="$refs.button"
     :checked=="multiple ? value : []" @input="setValue" @leave="showMenu = false"
   )
     slot

commit ece841de752ff8ed03fcb60bfc9be94dcb3a4e42

oops, 243de32889bde1a9baac76969fd20d1771bbf118 broke dropdown menus

source/upload/components/dropdown-menu.vue
@@ -33,7 +33,7 @@ export default
       f = if add then document.addEventListener else document.removeEventListener
       f 'click', @onDocumentClick, true
     onDocumentClick: (event) ->
-      if @show && !@button?.contains(event.target)
+      if @show && !@button?.contains(event.target) && !@$el.contains(event.target)
         event.preventDefault()
         @$emit 'leave'
   watch: